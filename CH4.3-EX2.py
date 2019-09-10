b235562d53e5b4f92c94ed088e71022f5a480e27
sevilay bayatli <sevilaybayatli@gmail.com>
2019-09-10 19:36:55 +0300

    first example in python

---
+++ CH4.3-EX2.py
@@ -0,0 +1,29 @@
+import turtle
+bob = turtle.Turtle()
+#print(bob)
+#for i in range(4):
+     #bob.fd(100)
+     #bob.lt(90)
+#turtle.mainloop()
+th=100
+
+def square(t,length):
+    #t=turtle.Turtle()
+    print(t)
+    for i in range(4):
+        t.fd(100+length)
+        t.lt(90)
+
+def sud(bob,th):
+    square(bob,th)
+#square(bob)
+sud(bob,th)
+turtle.mainloop()
+
+
+    
+
+
+    
+    
+
