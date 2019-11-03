f72b99b92c6a74e3f127994a7fb6fda06ed3d283
sevilay bayatli <sevilaybayatli@gmail.com>
2019-11-03 23:33:31 +0300

    new example of numpy

---
+++ Ex33.py
@@ -0,0 +1,8 @@
+# Question 3: Following is the provided numPy array. return array of items in the third column from all rows
+
+import numpy
+
+sampleArray=numpy.array([[1,56,3],[4,44,7],[8,23,4]])
+print(sampleArray)
+print("return array of items in the third column from all rows: ")
+print(sampleArray [:,1])
