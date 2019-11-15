9db184fa852881c190938079314ccce9bcd6d0ee
sevilay bayatli <sevilaybayatli@gmail.com>
2019-11-15 14:51:16 +0300

    to check whether a file exist

--- Ex68.py
+++ Ex68.py
@@ -1,12 +1,45 @@
 import sys
+import math
 #Write a Python program to add two objects if both objects are an integer type.
-def add_numbers(a,b):
-    if not (isinstance(a, int)and isinstance(b, int)):
-         raise TypeError("inputs must be intergers")
+##def add_numbers(a,b):
+ #   if not (isinstance(a, int)and isinstance(b, int)):
+  #       raise TypeError("inputs must be intergers")
  
-    else:
-      return a+b
+   # else:
+    #  return a+b
 
 #a=int(input("enter a number "))
 #b=int(input("enter a number "))
-print(add_numbers(10,2.4))
+#print(add_numbers(10,2.4))
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#def personal_detailes():
+#   name,age="sevilay",34
+#   address="Istanbul, Turkey"
+
+#  print("name:{}\n age:{}\n address:{}".format(name, age, address))
+
+
+#personal_detailes()
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#Write a Python program to solve 
+#def equation_solver(x,y):
+#     z=(x + y) * (x + y)
+#     print(z)
+
+
+#equation_solver(3,4)
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#Write a Python program to compute the distance between the points (x1, y1) and (x2, y2).
+
+p1=[4,0]
+p2=[6,6]
+
+#distance=math.sqrt(((p1[0]-p2[0])**2)+((p1[1]-p2[1])**2))
+distance = math.sqrt( ((p1[0]-p2[0])**2)+((p1[1]-p2[1])**2) )
+print(distance)
+
+
+   
+
+
+   
