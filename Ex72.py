ea6f6f07ede2902940614e2623743a72a09fe00e
sevilay bayatli <sevilaybayatli@gmail.com>
2019-11-21 15:09:33 +0300

    sum over container and concatenate a list of strings

--- Ex72.py
+++ Ex72.py
@@ -1,5 +1,15 @@
 import sys
 #Write a python program to sum of the first n positive integers.
-n=int(input("enter a number: "))
-sum=(n*(n+1))/2
-print(sum)  
+#n=int(input("enter a number: "))
+#sum=(n*(n+1))/2
+#print(sum)
+#Write a Python program to convert height (in feet and inches) to centimeters
+n1=float(input(enter a number in inces))
+n2=float(input(enter a number in feet))
+feet=30.48
+ince= 2.54
+hight=n1*feet+n2*ince
+print(hight)
+
+  
+
---
+++ Ex74.py
@@ -0,0 +1,15 @@
+import sys
+# Write a Python program to concatenate N strings.
+
+listColors=['red','black','white']
+
+z='-'.join(listColors)
+
+print("the colors "+z)
+
+# Write a Python program to calculate the sum over a container
+
+ss=sum([20,30,10])
+print("\n the sum of container= ", ss)
+print()	
+
