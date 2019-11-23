27ad404b5d58a4f32a2e39e9cd0bf4fd5fe649a4
sevilay bayatli <sevilaybayatli@gmail.com>
2019-11-23 15:36:26 +0300

    calculate product of list without a for loop

--- Ex81.py
+++ Ex81.py
@@ -1,4 +1,5 @@
 import sys
+from functools import reduce
 #Write a Python program to filter the positive numbers from a list.
 lls=[23,1,0,-1,-23]
 zz=['w','z','l','k']
@@ -13,5 +14,15 @@ print(nl)
 for j in zz:
     nz.append(j)
 print(nz)
+
+s=[2,3,6]
+print(s[0]*s[0]+s[1]*s[1]+s[2]*s[2])
+print(s[1]*s[1])
+print(s[2]*s[2])
+nums=[4,7,8]
+print(nums)
+print()
+product_list=reduce((lambda x,y:x*y),nums)
+print(product_list)
    
     
