c1a8ad970767e878734178047a0b3fc3a2473a26
sevilay bayatli <sevilaybayatli@gmail.com>
2019-11-23 15:06:22 +0300

    filter a list

---
+++ Ex81.py
@@ -0,0 +1,17 @@
+import sys
+#Write a Python program to filter the positive numbers from a list.
+lls=[23,1,0,-1,-23]
+zz=['w','z','l','k']
+nl=[]
+nz=[]
+for i in lls:
+    if (i>0):
+       nl.append(i)
+
+print(nl)
+
+for j in zz:
+    nz.append(j)
+print(nz)
+   
+    
