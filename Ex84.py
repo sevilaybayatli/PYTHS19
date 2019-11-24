1a91c11a3c30ef7d62a49d0cebeb80b254a52dd8
sevilay bayatli <sevilaybayatli@gmail.com>
2019-11-24 12:47:08 +0300

    operating system name, platform release and name

---
+++ Ex84.py
@@ -0,0 +1,10 @@
+import sys
+import os, platform
+#Write a Python program to find the operating system name, platform and platform release date.
+print("the opersating system name")
+print(os.name)
+print("the platform system")
+print(platform.system())
+print("the platform release")
+print(platform.release())
+
