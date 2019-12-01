79f1ed195ae1690f4156189cd97bad16b3057c73
sevilay bayatli <sevilaybayatli@gmail.com>
2019-12-01 20:21:19 +0300

    calculate the median of numbers

---
+++ Ex94.py
@@ -0,0 +1,32 @@
+import sys
+# Write a Python program to check the priority of the four operators (+, -, *, /).
+
+__operators__="+-*/"
+__paranthesis__="()"
+__priority__={'+':0,'-':0, '*':1, '/':1}
+
+def high_priority(operator1,operator2):
+    return __priority__[operator1] >= __priority__[operator2]
+
+
+print(high_priority('+','-'))
+print(high_priority('*','-'))
+print(high_priority('+','/'))
+
+#Write a Python program to find the median among three given numbers.
+
+nums=[2,5,1]
+numb=[2,3,4,1]
+
+def median(n):
+   if (len(n)%2>0):
+       z=int(len(n)/2)
+       return z
+   elif (len(n)%2==0):
+       z=int(len(n)/2)
+       m=z+n[z-1]/2
+       return m
+
+print("the median number of these numbers is: ", median(nums))
+print("the median number of these numbers is: ", median(numb))
+
