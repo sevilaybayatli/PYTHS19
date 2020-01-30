9537b721434c89091bdfc12702f5e02f6c25640e
sevilay bayatli <sevilaybayatli@gmail.com>
2020-01-30 22:35:47 +0300

    new function summing two integers

---
+++ Ex96.py
@@ -0,0 +1,15 @@
+import sys
+
+def sumsval(x, y):
+   z=x+y
+ 
+
+   return z
+
+x=int(input("enter a first number"))
+y=int(input("enter a second number"))
+
+print("the result= ", sumsval(x,y))
+
+
+
